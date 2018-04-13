0a034367d1c2a48144029038f974b9ac08317202
Eva Pamfil <eva.pamfil@mpublicite.fr>
2018-04-13 17:44:14 +0200

    add script modal

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -14,5 +14,55 @@ btnCOnnexion.addEventListener('click', function () {
 
 close.addEventListener('click', function () {
     modal.style.display="none";
-    main.classList.remove('blur');
+    
 },false);
+
+
+var elems = document.querySelectorAll('[data-modal]');
+var modals = {};
+var close = document.querySelectorAll('.close');
+
+
+    for(var i =0; i<elems.length; i++){
+        elems[i].style.display="none";
+        modals[elems[i].getAttribute('data-modal')] = elems[i];
+    }
+
+    var btns = document.querySelectorAll('[data-to]');
+
+    document.querySelector('body').addEventListener('click', handleClick, false);
+
+    function showModal(elem){
+        elem.style.display = "block";
+        main.classList.add('blur');
+    }
+
+ function handleClick(e) {
+        var btn = e.target.closest('[data-to]');
+        if(!btn){
+            return
+        }
+
+        var identifiant = btn.getAttribute('data-to');
+        if(!identifiant){
+            console.warn('Pas de suite');
+            return
+        }
+
+        var elem = modals[identifiant];
+        showModal(elem);
+
+    }
+
+for (var y=0; y<close.length; y++){
+  
+   function closeModal(e){
+     
+     var modal = e.target.closest('[data-modal]');
+     modal.style.display ="none";
+    main.classList.remove('blur');
+   }
+
+  close[y].addEventListener('click', closeModal, false);
+
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,55 @@
-var modal = document.querySelector('.modal'),
-    btnCOnnexion = document.querySelector('.button-co'),
-    btnInscription = document.querySelector('.button-inscription'),
-    main = document.querySelector('main'),
-    close = document.querySelector('.close')
+var main = document.querySelector('main'),
+var elems = document.querySelectorAll('[data-modal]')
+var modals = {},
+    current = null
+var close = document.querySelectorAll('.close')
 
-btnCOnnexion.addEventListener('click', function () {
-   if(modal.classList.contains('co')){
-       modal.style.display ="block";
-       main.classList.add('blur')
+
+    for(var i =0; i<elems.length; i++){
+        elems[i].style.display="none";
+        modals[elems[i].getAttribute('data-modal')] = elems[i]
+    }
+
+    var btns = document.querySelectorAll('[data-to]')
+
+    document.querySelector('body').addEventListener('click', handleClick, false);
+
+    function showModal(elem){
+        if(current){
+            //current.style.display = "none";
+        }
+
+        elem.style.display = "block";
+        current = elem
+        main.classList.add('blur')
+    }
+
+ function handleClick(e) {
+        var btn = e.target.closest('[data-to]')
+        if(!btn){
+            return
+        }
+
+        var identifiant = btn.getAttribute('data-to')
+        if(!identifiant){
+            console.warn('Pas de suite')
+            return
+        }
+
+        var elem = modals[identifiant];
+        showModal(elem)
+
+    }
+
+for (var y=0; y<close.length; y++){
+  
+   function closeModal(e){
+     
+     var modal = e.target.closest('[data-modal]')
+     modal.style.display ="none"
+    main.classList.remove('blur')
    }
 
-}, false)
+  close[y].addEventListener('click', closeModal, false)
 
-close.addEventListener('click', function () {
-    modal.style.display="none"
-    main.classList.remove('blur')
-},false)
+}
